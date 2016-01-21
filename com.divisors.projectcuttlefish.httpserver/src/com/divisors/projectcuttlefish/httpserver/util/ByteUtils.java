1e5f77f81dcf2eff087b59b2a4aa2c1a3bf08ea0
mailmindlin <liamttlyrox@gmail.com>
2016-01-20 22:20:06 -0500

    Documented ByteBufferTokenizer, efficiency improvements

--- com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/util/ByteUtils.java
+++ com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/util/ByteUtils.java
@@ -10,12 +10,27 @@ import java.nio.ByteBuffer;
 public class ByteUtils {
 	/**
 	 * Lets you do stuff like {@link String#split(String)}, but with ByteBuffers
+	 * 
+	 * TODO just a random idea, but this *might* be a lot faster if implemented natively.
+	 *  Just an idea.
 	 * @author mailmindlin
 	 */
 	public static final class ByteBufferTokenizer {
+		/**
+		 * Default capacity for buffer
+		 */
 		public static final int DEFAULT_CAPACITY = 8196;
+		/**
+		 * Token to search for
+		 */
 		protected final byte[] token;
+		/**
+		 * Buffer to search in
+		 */
 		protected final ByteBuffer buffer;
+		/**
+		 * Offset from the start of the buffer to continue searching
+		 */
 		protected int offset = 0;
 		public ByteBufferTokenizer(byte[] token) {
 			this(token, ByteBuffer.allocate(DEFAULT_CAPACITY));
@@ -24,18 +39,29 @@ public class ByteUtils {
 			this(token, ByteBuffer.allocate(capacity));
 		}
 		/**
-		 * 
+		 * Create with token and buffer
+		 * @param token token to search for
+		 * @param buffer haystack
 		 */
 		public ByteBufferTokenizer(byte[] token, ByteBuffer buffer) {
 			this.token = token;
 			this.buffer = buffer;
 		}
+		/**
+		 * How many bytes are available in the buffer to search
+		 * @return that
+		 */
 		public int available() {
 			return buffer.remaining();
 		}
 		public ByteBuffer getBuffer() {
 			return this.buffer;
 		}
+		/**
+		 * Put given bytes in buffer
+		 * @param bytes data to put in
+		 * @return success
+		 */
 		public boolean put(byte...bytes) {
 			try {
 				buffer.put(bytes);
@@ -45,6 +71,11 @@ public class ByteUtils {
 				return false;
 			}
 		}
+		/**
+		 * Put given bytes in buffer.
+		 * @param bytes data to put in (unsigned 8 bit values)
+		 * @return success
+		 */
 		public boolean put(int...bytes) {
 			byte[] arr = new byte[bytes.length];
 			for (int i=0;i<bytes.length;i++)
@@ -67,21 +98,27 @@ public class ByteUtils {
 			return this;
 		}
 		/**
-		 * ByteBuffer containing segment of next token parsed.
-		 * @return
+		 * ByteBuffer containing segment from the current position to next token parsed. The ByteBuffer returned
+		 * will contain the bytes matching the token at the end.
+		 * 
+		 * @return a mirror ByteBuffer containing a section of this. Any changes to this buffer's content will be reflected in the original
+		 *         buffer. If no match is found, then this method returns null.
 		 */
 		public ByteBuffer next() {
 			synchronized (this.buffer) {
 				ByteBuffer mirror = buffer.duplicate();
 				mirror.limit(mirror.position()).position(offset);
-				//TODO optimize for speed
+				//small buffer for reading tokens
+				final byte[] minibuf;
+				if (token.length > 1)
+					minibuf = new byte[token.length - 1];
+				else
+					minibuf = null;
 				search:
 				while (mirror.remaining() >= token.length) {
-					byte b = mirror.get();
-					if (b == token[0]) {
+					if (mirror.get() == token[0]) {
 						if (token.length > 1) {
 							mirror.mark();
-							byte[] minibuf = new byte[token.length - 1];
 							mirror.get(minibuf);
 							for (int i=0; i < minibuf.length; i++)
 								if (this.token[i+1] != minibuf[i]) {
@@ -90,7 +127,7 @@ public class ByteUtils {
 								}
 						}
 						int size = mirror.position() - offset;
-						mirror.position(offset).limit(offset + size);
+						mirror.flip().position(offset);
 						offset += size;
 						return mirror;
 					}
@@ -99,7 +136,7 @@ public class ByteUtils {
 			return null;
 		}
 		/**
-		 * Return a buffer containing whatever bytes have not yet been tokenized.
+		 * @return a buffer containing whatever bytes have not yet been tokenized.
 		 */
 		public ByteBuffer remaining() {
 			ByteBuffer mirror = buffer.duplicate().asReadOnlyBuffer();
@@ -107,6 +144,11 @@ public class ByteUtils {
 			return mirror;
 		}
 	}
+	/**
+	 * 
+	 * @param buffer
+	 * @return
+	 */
 	public static byte[] toArray(ByteBuffer buffer) {
 		byte[] result = new byte[buffer.remaining()];
 		buffer.get(result);
