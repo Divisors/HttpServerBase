f28f0479e3b988c1ef0fbd5e584e225faf0d0e5a
mailmindlin <liamttlyrox@gmail.com>
2016-01-20 22:00:38 -0500

    Updated TCP implementations
    Now with a stateful API, life works *much* better. Between that, better
    debugging, and bugfixes, I ended up rewriting almost every line of code
    (at least it felt like that).
    
    I have to say, however, that I'm pretty proud of this code. It's
    surprisingly robust, and works how it should.

--- com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/api/tcp/TcpChannelImpl.java
+++ com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/api/tcp/TcpChannelImpl.java
@@ -13,14 +13,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.divisors.projectcuttlefish.httpserver.api.Channel;
-import com.divisors.projectcuttlefish.httpserver.api.Codec;
+import com.divisors.projectcuttlefish.httpserver.api.ChannelOption;
 
 import reactor.bus.Event;
 import reactor.bus.registry.Registration;
 import reactor.fn.Consumer;
 
 /**
- * Implementation of {@link TcpChannel}. 
+ * Implementation of {@link TcpChannel}.
+ * 
  * @author mailmindlin
  * @see TcpChannel
  */
@@ -31,57 +32,66 @@ public class TcpChannelImpl implements TcpChannel {
 	protected final SocketChannel socket;
 	/**
 	 * Server that this channel is from
+	 * 
 	 * @see #getServer()
 	 */
 	protected final TcpServerImpl server;
 	/**
 	 * Connection id
+	 * 
 	 * @see #getConnectionID()
 	 */
 	protected final long connID;
-	protected final List<Registration<Object,Consumer<? extends Event<?>>>> subscriptions = new ArrayList<>();
-	protected final Deque<ByteBuffer> writeQueue = new LinkedList<>();//TODO use better queue
+	protected final List<Registration<Object, Consumer<? extends Event<?>>>> subscriptions = new ArrayList<>();
+	//TODO use better queue
+	protected final Deque<ByteBuffer> writeQueue = new LinkedList<>();
+
 	/**
 	 * Create channel from server, wrapping socket, with id.
-	 * @param server parent server
-	 * @param socket socket to wrap
-	 * @param id connection id
+	 * 
+	 * @param server
+	 *            parent server
+	 * @param socket
+	 *            socket to wrap
+	 * @param id
+	 *            connection id
 	 */
 	public TcpChannelImpl(TcpServerImpl server, SocketChannel socket, final long id) {
 		this.server = server;
 		this.socket = socket;
 		this.connID = id;
 	}
-
+	
 	@Override
 	public boolean isOpen() {
 		return this.socket.isOpen();
 	}
-
+	
 	@Override
 	public long getConnectionID() {
 		return connID;
 	}
+	
 	/**
 	 * Close this channel, canceling any event listeners registered through this
 	 */
 	@Override
 	public void close() throws IOException {
-		//DEBUG
+		// DEBUG
 		try {
-			StringBuilder sb = new StringBuilder("Closing tcp channel #")
-				.append(this.getConnectionID());
-				SocketAddress addr = getRemoteAddress();
+			StringBuilder sb = new StringBuilder("TCP::Closing tcp channel #").append(this.getConnectionID());
+			SocketAddress addr = getRemoteAddress();
 			if (addr != null)
-				sb.append(" connected to ")
-					.append(addr);
+				sb.append(" connected to ").append(addr);
 			System.out.println(sb.toString());
-		} catch (Exception e){}
+		} catch (Exception e) {
+		}
+		this.server.channelMap.remove(this.getConnectionID());
 		this.writeQueue.clear();
 		this.subscriptions.forEach(Registration::cancel);
 		this.socket.close();
 	}
-
+	
 	@Override
 	public TcpServerImpl getServer() {
 		return server;
@@ -96,36 +106,38 @@ public class TcpChannelImpl implements TcpChannel {
 			return null;
 		}
 	}
+	
 	/**
 	 * TODO stackoverflow.com/questions/10940654/java-socketchannel-register-for-multiple-op-codes-is-never-selected#comment14277475_10940910
 	 */
 	@Override
 	public TcpChannelImpl write(ByteBuffer data) {
-		System.out.println("Queueing "+data.remaining() + " bytes for writing #"+this.getConnectionID() + this.isOpen());
+		System.out.println("Queueing " + data.remaining() + " bytes for writing #" + this.getConnectionID());
 		this.writeQueue.add(data);
 		SelectionKey key = this.socket.keyFor(this.getServer().selector);
 		key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
-		System.out.println("" + key.isValid() + key.interestOps());
-		//notify the selector of the interest
-		this.getServer().selector.wakeup();//this decreased latency by like >5000% (really), because writes were being blocked until the another socket triggered an event
+		// notify the selector of the interest
+		this.getServer().selector.wakeup();// this decreased latency by like
+											// >5000% (really), because writes
+											// were being blocked until the
+											// another socket triggered an event
 		return this;
 	}
+	
 	@Override
 	public TcpChannelImpl onRead(Consumer<ByteBuffer> handler) {
-		this.subscriptions.add(server.bus.on($t("tcp.read",this.getConnectionID()),event->handler.accept((ByteBuffer)event.getData())));
+		this.subscriptions.add(server.bus.on($t("tcp.read", this.getConnectionID()), event -> handler.accept((ByteBuffer) event.getData())));
 		return this;
 	}
-	@Override
-	public <X, Y> Channel<X, Y> map(Codec<ByteBuffer, ByteBuffer, X, Y> codec) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	protected int doWrite() throws IOException {
-		System.out.println("Will write..");
+		System.out.println("\tWill write...");
+		ByteBuffer buf;
 		synchronized (this.writeQueue) {
-			ByteBuffer buf = writeQueue.poll();
-			if (buf == null)
+			buf = writeQueue.poll();
+			if (buf == null) {
+				System.out.println("\tEmpty buffer (nothing to write)");
 				return 0;
+			}
 			// If the buffer taken is really small, and there is another available, take it too.
 			if (buf.remaining() < 1024 && !writeQueue.isEmpty()) {
 				ByteBuffer buf2 = writeQueue.poll();
@@ -142,7 +154,7 @@ public class TcpChannelImpl implements TcpChannel {
 			if (written < 0)
 				return written;
 			
-			System.out.println("Wrote "+written + "/"+(written + buf.remaining()) + " bytes to #" + this.getConnectionID());
+			System.out.println("\tWrote " + written + "/" + (written + buf.remaining()) + " bytes to #" + this.getConnectionID());
 			//not everything was written from the buffer, so queue it for writing next time
 			if (buf.remaining() > 0)
 				this.writeQueue.addFirst(buf);
@@ -154,5 +166,11 @@ public class TcpChannelImpl implements TcpChannel {
 			}
 			return written;
 		}
+	}
+
+	@Override
+	public <E> Channel<ByteBuffer, ByteBuffer> setOption(ChannelOption<E> key, E value) {
+		// TODO Auto-generated method stub
+		return this;
 	}
 }
--- com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/api/tcp/TcpServerImpl.java
+++ com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/api/tcp/TcpServerImpl.java
@@ -8,23 +8,23 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.StandardSocketOptions;
 import java.nio.ByteBuffer;
-import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.time.Duration;
+import java.time.Instant;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-import org.reactivestreams.Processor;
-
-import com.divisors.projectcuttlefish.httpserver.api.Server;
+import com.divisors.projectcuttlefish.httpserver.api.ServiceState;
+import com.divisors.projectcuttlefish.httpserver.util.CancellingHandler;
 
 import reactor.bus.Event;
 import reactor.bus.EventBus;
@@ -40,20 +40,20 @@ public class TcpServerImpl implements TcpServer, Runnable {
 	protected EventBus bus;
 	protected final ConcurrentHashMap<Long, TcpChannelImpl> channelMap = new ConcurrentHashMap<>();
 	protected final AtomicLong nextId = new AtomicLong(0L);
-	protected final Selector selector;
+	protected Selector selector;
 	/**
 	 * Server socket
 	 */
-	protected final ServerSocketChannel server;
+	protected ServerSocketChannel server;
 	/**
 	 * Address to connect to
 	 */
 	protected final SocketAddress addr;
 	/**
-	 * E
+	 * Executor that this server runs on
 	 */
 	protected ExecutorService executor;
-	protected final AtomicBoolean running = new AtomicBoolean(false);
+	protected final AtomicReference<ServiceState> state = new AtomicReference<>(ServiceState.UNINITIALIZED);
 	protected ByteBuffer readBuffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
 	
 	
@@ -62,32 +62,55 @@ public class TcpServerImpl implements TcpServer, Runnable {
 	}
 	public TcpServerImpl(SocketAddress addr) throws IOException {
 		this.addr = addr;
-		System.out.println("Binding to " + addr.toString());
+		System.out.println("TCP::Binding to " + addr.toString());
+	}
+	@Override
+	public TcpServerImpl init() throws Exception {
+		if (getState() != ServiceState.UNINITIALIZED)
+			throw new IllegalStateException("Expected: UNITNITIALIZED; State: "+getState().name());
 		selector = Selector.open();
 		server = ServerSocketChannel.open();
 		server.socket().bind(addr);
 		server.configureBlocking(false);
 		server.register(selector, SelectionKey.OP_ACCEPT);
+		state.set(ServiceState.INITIALIZED);
+		return this;
+	}
+	@Override
+	public TcpServerImpl start() {
+		if (!state.compareAndSet(ServiceState.INITIALIZED, ServiceState.STARTING))
+			throw new IllegalStateException("Expected: INITIALIZED; State: "+state.get().name());
+		if (executor == null) {
+			run();
+		} else {
+			executor.submit(this);
+		}
+		return this;
 	}
 	@Override
 	public TcpServerImpl start(Consumer<? super TcpServer> initializer) throws IOException, IllegalStateException {
+		if (!state.compareAndSet(ServiceState.INITIALIZED, ServiceState.STARTING))
+			throw new IllegalStateException("Expected: INITIALIZED; State: "+state.get().name());
 		initializer.accept(this);
-		return this.start();
-	}
-	public TcpServerImpl start() {
-		if (executor == null)
+		if (executor == null) {
 			run();
-		else
+		} else {
 			executor.submit(this);
+		}
 		return this;
 	}
-	public TcpServerImpl dispatchOn(Processor<Event<?>, Event<?>> p) {
-		this.bus = EventBus.create(p);
+	@Override
+	public TcpServerImpl dispatchOn(EventBus bus) {
+		this.bus = bus;
 		return this;
 	}
+	@Override
 	public TcpServerImpl runOn(ExecutorService executor) {
-		if (!running.get())
+		final ServiceState cstate = getState();
+		if (cstate == ServiceState.UNINITIALIZED || cstate == ServiceState.INITIALIZED || cstate == ServiceState.STARTING)
 			this.executor = executor;
+		else
+			throw new IllegalStateException("Expected: INITIALIZED or STARTING; State: "+cstate.name());	
 		return this;
 	}
 	@Override
@@ -96,46 +119,47 @@ public class TcpServerImpl implements TcpServer, Runnable {
 	}
 
 	@Override
-	public boolean isRunning() {
-		return running.get();
-	}
-
-	@Override
 	public boolean isSecure() {
-		return false;
+		return false;//not TLS
 	}
-
-	@SuppressWarnings("unchecked")
+	
 	@Override
+	@SuppressWarnings("unchecked")
 	public TcpServer onConnect(Consumer<TcpChannel> handler) {
 		this.bus.on($("tcp.connect"), event -> handler.accept(((Event<TcpChannel>)event).getData()));
 		return this;
 	}
-
+	
 	@Override
-	public boolean shutdownNow() throws IOException, InterruptedException {
-		this.running.set(false);
-		this.server.close();
-		this.selector.close();
-		System.out.println("Bye!");
-		this.executor.shutdownNow();
-		return true;
+	@SuppressWarnings("unchecked")
+	public TcpServerImpl onConnect(Predicate<TcpChannel> handler) {
+		CancellingHandler.bind(this.bus, $("tcp.connect"), event->(handler.test(((Event<TcpChannel>)event).getData())));
+		return this;
 	}
+	
+	@Override
 	public void run() {
-		if (!running.weakCompareAndSet(false, true))
-			throw new IllegalStateException("Was already running!");
+		if (!state.compareAndSet(ServiceState.STARTING, ServiceState.RUNNING))
+			throw new IllegalStateException("I was already running! (Expect: ServiceState#STARTING; was " + state.get() + ").");
 		try {
-			while (true) {
+			ServiceState cstate;//temporary state mirror for making life easy
+
+			while ((cstate = getState()) == ServiceState.RUNNING || (cstate == ServiceState.STOPPING && channelMap.size() > 0)) {
 				try {
-					System.out.println("Polling...");
-					int nKeys = selector.select();
+					int nKeys = 0;//number of keys selected
+					System.out.println("TCP::Polling... ("+this.channelMap.size()+" open)");
+					if ((cstate = getState()) == ServiceState.STOPPING)
+						nKeys = selector.select(1000);//the server is stopping now, so ensure that this doesn't hang for as long.
+					else
+						nKeys = selector.select();//there are some open channels & the server isn't closing, so we don't have to check as much
+					
 					if (nKeys > 0) {
-						System.out.println(""+nKeys+ " keys");
+						System.out.println("\tGot " + nKeys + " key(s)");
 						Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
 						while (keyIterator.hasNext()) {
 							SelectionKey key = keyIterator.next();
 							keyIterator.remove();
-							System.out.println(key.toString() + " | " + key.interestOps() + " |V" + key.isValid() + " |A" + key.isAcceptable() + " |R" + key.isReadable() + " |W" + key.isWritable() + " |C" + key.isConnectable());
+							System.out.println("\tKey : " + key.interestOps() + " |" + (key.isValid()?" Valid":"") + (key.isAcceptable()?" Acceptable":"") + (key.isReadable()?" Readable":"") + (key.isWritable()?" Writable":"") + (key.isConnectable()?" Connectable":""));
 							if (!key.isValid())
 								continue;
 							
@@ -151,15 +175,63 @@ public class TcpServerImpl implements TcpServer, Runnable {
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
-					return;
 				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if (!running.weakCompareAndSet(true, false))
+			if (!(this.state.compareAndSet(ServiceState.RUNNING, ServiceState.INITIALIZED) || this.state.compareAndSet(ServiceState.STOPPING, ServiceState.INITIALIZED)))
 				throw new IllegalStateException("...I'm not even sure what caused this...");
+			this.state.notifyAll();
 		}
+	}
+	
+	@Override
+	public boolean shutdown() {
+		if (this.state.compareAndSet(ServiceState.RUNNING, ServiceState.STOPPING) || getState() == ServiceState.STOPPING) {
+			this.selector.wakeup();
+			return true;
+		}
+		return false;
+	}
+	@Override
+	public boolean shutdown(Duration timeout) throws InterruptedException {
+		if(!this.state.compareAndSet(ServiceState.RUNNING, ServiceState.STOPPING) && getState() != ServiceState.STOPPING)
+			return true;
+		this.selector.wakeup();
+		Instant now = null, end = Instant.now().plus(timeout);
+		while (getState() == ServiceState.STOPPING && end.isAfter(now = Instant.now())) {
+			Duration remaining = Duration.between(now, end);
+			this.state.wait(remaining.toMillis(), remaining.getNano() % 1000);
+		}
+		return getState() != ServiceState.STOPPING;//NOTE: should this also check if the state is RUNNING?
+	}
+	@Override
+	public boolean shutdownNow() throws IOException, InterruptedException {
+		if ((!this.state.compareAndSet(ServiceState.RUNNING, ServiceState.STOPPING)) && getState() != ServiceState.STOPPING)
+			return false;
+		this.selector.wakeup();
+		this.executor.shutdownNow();
+		final boolean result = this.executor.awaitTermination(20, TimeUnit.MILLISECONDS);
+		this.state.compareAndSet(ServiceState.STOPPING, ServiceState.INITIALIZED);
+		System.out.println("Bye!");
+		return result;
+	}
+	@Override
+	public void destroy() throws RuntimeException {
+		try {
+			this.server.close();
+			this.server = null;
+			this.selector.close();
+			this.selector = null;
+			this.channelMap.clear();
+			this.executor.shutdownNow();
+			this.executor = null;
+		} catch (IOException e) {
+			this.state.set(ServiceState.DESTROYED);
+			throw new RuntimeException(e);
+		}
+		this.state.set(ServiceState.UNINITIALIZED);
 	}
 	/**
 	 * Accept an incoming connection.
@@ -167,23 +239,40 @@ public class TcpServerImpl implements TcpServer, Runnable {
 	 * @throws IOException if there was a problem setting it up
 	 */
 	protected void accept(SelectionKey key) throws IOException {
-		System.out.println("Accepting...");
+		System.out.println("TCP::Accepting...");
 		SocketChannel socket = server.accept();
+		
+		//if shutting down, don't accept any new threads
+		if (getState() == ServiceState.STOPPING) {
+			socket.close();
+			key.cancel();
+			System.out.println("\tRejected thread (Shutting down).");
+			return;
+		}
+		
+		//setup socket
 		socket.configureBlocking(false);
 		socket.setOption(StandardSocketOptions.SO_KEEPALIVE, true);
 		socket.setOption(StandardSocketOptions.TCP_NODELAY, true);//We should *hopefully* be writing large amounts of data at a time, so this just decreases the RTT
 		
+		//create channel
 		long id = this.nextId.incrementAndGet();
 		TcpChannelImpl channel = upgradeSocket(socket, id);
 		channelMap.put(id, channel);
-		bus.notify("tcp.connect",Event.<TcpChannel>wrap(channel));
 		System.out.println("\tAccepted from "+channel.getRemoteAddress().toString());
 		System.out.println("\tID #"+id);
 		
+		//trigger handlers
+		Event.Headers eventHeaders = new Event.Headers();
+		eventHeaders.setOrigin("TcpServerImpl@"+addr.toString());
+		bus.notify("tcp.connect",new Event<TcpChannel>(eventHeaders, channel));
+		
+		//queue for future I/O
 		socket.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE, id);
-		System.out.println("\tDone Accepting");
+		System.out.println("\tDone.");
 	}
 	protected TcpChannelImpl upgradeSocket(SocketChannel socket, long id) {
+		//this method can be overridden by children of this class offering encryption & stuff
 		return new TcpChannelImpl(this, socket, id);
 	}
 	/**
@@ -192,21 +281,27 @@ public class TcpServerImpl implements TcpServer, Runnable {
 	 * @throws IOException
 	 */
 	protected void read(SelectionKey key) throws IOException {
+		//get socket & channel
 		long id = (Long)key.attachment();
-		System.out.println("Reading #" + id + "...");
+		System.out.println("TCP::Reading #" + id + "...");
 		TcpChannelImpl channel = this.channelMap.get(id);
 		SocketChannel socket = (SocketChannel)key.channel();
 		
-		this.readBuffer.clear();
 		//read from socket
-		int read = socket.read(this.readBuffer);
-		
-		if (read < 0) {
-			//close channel
-			System.out.println("\tClosing channel...");
+		this.readBuffer.clear();
+		int read;//number of bytes read
+		try {
+			read = socket.read(this.readBuffer);
+			//if read<0, then the channel is closed
+			if (read < 0) {
+				//close channel
+				System.out.println("\tClosing channel...");
+				channel.close();
+				return;
+			}
+		} catch (IOException e) {
 			channel.close();
-			this.channelMap.remove(channel.getConnectionID());
-			return;
+			throw e;
 		}
 		
 		this.readBuffer.flip();
@@ -216,6 +311,7 @@ public class TcpServerImpl implements TcpServer, Runnable {
 		buffer.flip();
 		
 		//send event
+		System.out.println("TCP::Read " + read + " bytes from #" + id);
 		bus.notify(Tuple.<String,Long>of("tcp.read", channel.getConnectionID()), Event.<ByteBuffer>wrap(buffer));
 		
 		//FOR TESTING
@@ -225,56 +321,43 @@ public class TcpServerImpl implements TcpServer, Runnable {
 //		System.out.println(new String(arr));
 		
 		//register socket with selector
-		try {
-			socket.register(selector, SelectionKey.OP_READ, id);
-		} catch (ClosedChannelException e) {
-			this.channelMap.remove(id);
-			channel.close();
-			e.printStackTrace();
-			return;
+		if (!((SocketChannel)key.channel()).isOpen()) {
+			try {
+				channel.close();
+			} catch (IOException e) {
+				//the socket was already closed
+			}
+			channelMap.remove(id);
+		} else {
+			key.interestOps(SelectionKey.OP_READ);
 		}
 	}
 	protected void write(SelectionKey key) throws IOException {
 		Object attachment = key.attachment();
 		if (!key.isValid()) {
-			System.err.println("Invalid key"+attachment);
+			System.err.println("TCP::Invalid key" + attachment);
 			return;
 		}
 		long id = (Long)attachment;
-		System.out.println("Writing #" + id + "...");
+		System.out.println("TCP::Writing #" + id + "...");
 		
 		TcpChannelImpl channel = this.channelMap.get(id);
-		SocketChannel socket = (SocketChannel)key.channel();
 
 		channel.doWrite();
 		
-		try {
-			socket.register(selector, SelectionKey.OP_READ, id);
-		} catch (ClosedChannelException e) {
-			this.channelMap.remove(id);
-			channel.close();
-			e.printStackTrace();
-			return;
+		if (!((SocketChannel)key.channel()).isOpen()) {
+			try {
+				channel.close();
+			} catch (IOException e) {
+				//the socket was already closed
+			}
+			channelMap.remove(id);
+		} else {
+			key.interestOps(SelectionKey.OP_READ);
 		}
 	}
 	@Override
-	public Server<ByteBuffer, ByteBuffer, TcpChannel> onConnect(Predicate<TcpChannel> handler) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public boolean shutdown() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	@Override
-	public boolean shutdown(Duration timeout) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	@Override
-	public boolean isShuttingDown() {
-		// TODO Auto-generated method stub
-		return false;
+	public ServiceState getState() {
+		return this.state.get();
 	}
 }
