4b0d72c55473709bde510baa103309a207af0058
mailmindlin <liamttlyrox@gmail.com>
2016-01-26 12:26:32 -0500

    Worked on UA stuff

--- com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/ua/UserAgentDetector.java
+++ com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/ua/UserAgentDetector.java
@@ -1,7 +1,33 @@
 package com.divisors.projectcuttlefish.httpserver.ua;
 
-public class UserAgentDetector {
-	public UserAgent parse(String s) {
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserAgentDetector implements Function<String, UserAgent> {
+	List<UAPattern> patterns = new LinkedList<UAPattern>();
+	public void registerPattern(String name, String pattern) {
+		this.patterns.add(new UAPattern(name, pattern));
+	}
+	@Override
+	public UserAgent apply(String s) {
+		for (UAPattern pattern : patterns) {
+			Matcher m = pattern.pattern.matcher(s);
+			if (m.find()) {
+				System.out.println(pattern.name);
+				return null;
+			}
+		}
 		return null;
+	}
+	static class UAPattern {
+		public String name;
+		public Pattern pattern;
+		public UAPattern(String name, String pattern) {
+			this.name = name;
+			this.pattern = Pattern.compile(pattern);
+		}
 	}
 }
