4ac590e9153f394191ba6052a14115fe1e248f1d
mailmindlin <liamttlyrox@gmail.com>
2016-01-28 23:07:20 -0500

    Documented

--- com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/util/RegistrationCancelAction.java
+++ com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/util/RegistrationCancelAction.java
@@ -5,20 +5,38 @@ import java.lang.ref.WeakReference;
 import com.divisors.projectcuttlefish.httpserver.api.Action;
 import com.divisors.projectcuttlefish.httpserver.api.error.ActionUnavailableException;
 
-import reactor.bus.Event;
 import reactor.bus.registry.Registration;
-import reactor.fn.Consumer;
 
+/**
+ * An action for the cancellation of a registration.
+ * @author mailmindlin
+ */
 public class RegistrationCancelAction implements Action {
+	/**
+	 * A reference to the registration
+	 */
 	protected final WeakReference<Registration<?, ?>> registration;
+	/**
+	 * Create with given registration
+	 * @param registration registration to cancel
+	 */
 	public RegistrationCancelAction(Registration<?, ?> registration) {
 		this.registration = new WeakReference<>(registration);
 	}
+	/**
+	 * Whether this action is currently available. Will probably (assume always) return
+	 * true if the registration hasn't been cancelled.
+	 * @return the availability of this action
+	 */
 	@Override
 	public boolean isAvailable() {
 		Registration<?,?> registration = this.registration.get();
 		return !(registration == null || registration.isCancelled());
 	}
+	/**
+	 * Cancel the registration
+	 * @throws ActionUnavailableException if cancelling the registration isn't a thing that you can do now
+	 */
 	@Override
 	public void act() throws ActionUnavailableException {
 		Registration<?,?> registration = this.registration.get();
