047b467ad730f0b9833ab673ef895c3ef6c2dcf4
mailmindlin <liamttlyrox@gmail.com>
2016-01-28 23:54:13 -0500

    Added ParseException
    Custom exception *just for* whenever your parser doesn't work
    (surprisingly often).

---
+++ com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/api/error/ParseException.java
@@ -0,0 +1,17 @@
+package com.divisors.projectcuttlefish.httpserver.api.error;
+
+public class ParseException extends RuntimeException {
+	private static final long serialVersionUID = 6852954719652680474L;
+	public ParseException() {
+		super();
+	}
+	public ParseException(String message) {
+		super(message);
+	}
+	public ParseException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	public ParseException(Throwable cause) {
+		super(cause);
+	}
+}
