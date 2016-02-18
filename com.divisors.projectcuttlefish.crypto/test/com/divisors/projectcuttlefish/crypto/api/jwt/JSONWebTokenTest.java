bd4be3229d224cbebaf45281cced19f0c4c8ba42
mailmindlin <liamttlyrox@gmail.com>
2016-02-18 12:34:48 -0500

    Added simple test for JWT

---
+++ com.divisors.projectcuttlefish.crypto/test/com/divisors/projectcuttlefish/crypto/api/jwt/JSONWebTokenTest.java
@@ -0,0 +1,15 @@
+package com.divisors.projectcuttlefish.crypto.api.jwt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JSONWebTokenTest {
+
+	@Test
+	public void test() throws Exception {
+		JSONWebToken.parseJOSE("eyJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.");
+		fail("Not yet implemented");
+	}
+
+}
