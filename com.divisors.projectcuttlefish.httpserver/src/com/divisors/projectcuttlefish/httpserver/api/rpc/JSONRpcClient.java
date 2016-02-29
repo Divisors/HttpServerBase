df0593a850176c17d4557790256afcfabf961b10
mailmindlin <liamttlyrox@gmail.com>
2016-02-29 12:34:50 -0500

    Bugfixes & added functionality
    - Better method matching

--- com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/api/rpc/JSONRpcClient.java
+++ com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/api/rpc/JSONRpcClient.java
@@ -4,22 +4,20 @@ import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Future;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 
 import org.json.JSONObject;
 
 import com.divisors.projectcuttlefish.httpserver.api.request.HttpRequest;
 import com.divisors.projectcuttlefish.httpserver.api.response.HttpResponse;
-import com.sun.org.apache.bcel.internal.classfile.Field;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
@@ -62,34 +60,15 @@ public abstract class JSONRpcClient {
 		return pool.get(clazz.getName());
 	}
 	
-	protected Map<Class<? extends JSONRemote>, ClassInfo> metadata = new ConcurrentHashMap<>();
-	protected Map<Class<? extends JSONRemote>, Class<? extends JSONRemote>> proto = new ConcurrentHashMap<>();
-	
-	protected abstract Future<HttpResponse> doHttp(HttpRequest request);
-	
-	public <E extends JSONRemote> E get(String url, Class<E> clazz) {
-		//TODO finish
-		@SuppressWarnings("unchecked")
-		Class<E> prototype = (Class<E>) proto.get(clazz);
-		
-		E result;
-		
-		try {
-			result = prototype.getDeclaredConstructor(getClass(), String.class).newInstance(this, url);
-		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-			e.printStackTrace();
-			return null;
-		}
-		return result;
-	}
-	
-	protected ClassInfo getMeta(Class<? extends JSONRemote> clazz) {
-		
-		return null;
-	}
-	
-	protected Class<?> unbox(Class<?> primitive) {
-		if (primitive ==boolean.class)
+	/**
+	 * Get the class of the primitive type boxed. For example, if this method is called with <code>int.class</code>, it will return <code>Integer.class</code>
+	 * @param primitive the type to box
+	 * @return the boxed class, or the input if the input is not primitive
+	 * @see #unbox(Class)
+	 * @see #boxArray(Class)
+	 */
+	public static Class<?> box(Class<?> primitive) {
+		if (primitive == boolean.class)
 			return Boolean.class;
 		if (primitive == byte.class)
 			return Byte.class;
@@ -106,6 +85,32 @@ public abstract class JSONRpcClient {
 		if (primitive == double.class)
 			return Double.class;
 		return primitive;
+	}
+	/**
+	 * Get the type of the given type unboxed. For example, if this method is called with <code>Integer.class</code>, it will return <code>int.class</code>
+	 * @param boxed type to unbox
+	 * @return the primitive type, or the input type if it's not a boxed type
+	 * @see #box(Class)
+	 * @see #unboxArray(Class)
+	 */
+	public static Class<?> unbox(Class<?> boxed) {
+		if (boxed == Boolean.class)
+			return boolean.class;
+		if (boxed == Byte.class)
+			return byte.class;
+		if (boxed == Character.class)
+			return char.class;
+		if (boxed == Short.class)
+			return short.class;
+		if (boxed == Integer.class)
+			return int.class;
+		if (boxed == Long.class)
+			return long.class;
+		if (boxed == Float.class)
+			return float.class;
+		if (boxed == Double.class)
+			return double.class;
+		return boxed;
 	}
 	public static Class<?> getArrayClass(Class<?> componentType) throws ClassNotFoundException {
 	    ClassLoader classLoader = componentType.getClassLoader();
@@ -135,21 +140,61 @@ public abstract class JSONRpcClient {
 	    }
 	    return classLoader != null ? classLoader.loadClass(name) : Class.forName(name);
 	}
+	public static Class<?> getArrayClass(Class<?> componentType, int levels) throws ClassNotFoundException {
+	    ClassLoader classLoader = componentType.getClassLoader();
+	    String name;
+	    if(componentType.isArray()){
+	        // just add a leading "["
+	        name = "["+componentType.getName();
+	    }else if(componentType == boolean.class){
+	        name = "[Z";
+	    }else if(componentType == byte.class){
+	        name = "[B";
+	    }else if(componentType == char.class){
+	        name = "[C";
+	    }else if(componentType == double.class){
+	        name = "[D";
+	    }else if(componentType == float.class){
+	        name = "[F";
+	    }else if(componentType == int.class){
+	        name = "[I";
+	    }else if(componentType == long.class){
+	        name = "[J";
+	    }else if(componentType == short.class){
+	        name = "[S";
+	    }else{
+	        // must be an object non-array class
+	        name = "[L"+componentType.getName()+";";
+	    }
+	    //TODO make more efficient
+	    for (levels--; levels > 0; levels--)
+	    	name = "[" + name;
+	    return classLoader != null ? classLoader.loadClass(name) : Class.forName(name);
+	}
 	/**
 	 * Convert an
 	 * @param primitiveArray
 	 * @return
+	 * @see #unbox(Class)
+	 * @see #boxArray(Class)
 	 */
-	protected Class<?> unboxArray(Class<?> primitiveArray) {
+	public static Class<?> unboxArray(Class<?> primitiveArray) {
 		if (!primitiveArray.isArray())
 			return primitiveArray;
-		Class<?> primitive = primitiveArray.getComponentType();
-		if (primitive.isArray())
+		Class<?> primitive = primitiveArray;
+		if (primitive.isArray()) {
+			int levels;
+			for (levels = 1; primitive.isArray(); levels++)
+				primitive = primitive.getComponentType();
 			try {
-				return getArrayClass(unboxArray(primitive));
+				//loop for multidimensional arrays
+				return getArrayClass(unbox(primitive), levels);
 			} catch (ClassNotFoundException e1) {
 				e1.printStackTrace();
+				return null;
 			}
+		}
+		
 		if (!primitive.isPrimitive())
 			return primitiveArray;
 		try {
@@ -159,8 +204,96 @@ public abstract class JSONRpcClient {
 			return primitiveArray;
 		}
 	}
+	/**
+	 * Convert a primitive array to an array of the boxed type of that primitive. Multidimensional arrays supported.
+	 * <br/>
+	 * TODO maybe merge with {@link #box(Class)}
+	 * @param primitiveArray
+	 * @return
+	 * @see #box(Class)
+	 * @see #unboxArray(Class)
+	 */
+	public static Class<?> boxArray(Class<?> array) {
+		if (!array.isArray())
+			return array;
+		
+		Class<?> component = array;
+		if (component.isArray()) {
+			int levels;
+			for (levels = 1; component.isArray(); levels++)
+				component = component.getComponentType();
+			try {
+				//loop for multidimensional arrays
+				return getArrayClass(box(component), levels);
+			} catch (ClassNotFoundException e1) {
+				e1.printStackTrace();
+				return null;
+			}
+		}
+		
+		if (component.isPrimitive())
+			return array;
+		
+		try {
+			return getArrayClass(box(component));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return array;
+		}
+	}
 	
-	protected Set<Method> getMethodBySignature(Class<?> clazz, String methodName, String[] args, Set<String> allowedExceptions) {
+	protected Map<Class<? extends JSONRemote>, ClassInfo<?>> metadata = new ConcurrentHashMap<>();
+	protected Map<Class<? extends JSONRemote>, Class<? extends JSONRemote>> proto = new ConcurrentHashMap<>();
+	
+	protected abstract Future<HttpResponse> doHttp(HttpRequest request);
+	
+	public <E extends JSONRemote> E get(String url, Class<E> clazz) {
+		//TODO finish
+		@SuppressWarnings("unchecked")
+		Class<E> prototype = (Class<E>) proto.get(clazz);
+		
+		E result;
+		
+		try {
+			result = prototype.getDeclaredConstructor(getClass(), String.class).newInstance(this, url);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return result;
+	}
+	
+	protected <E extends JSONRemote> ClassInfo<E> getMeta(Class<E> clazz) {
+		ClassInfo<E> result = new ClassInfo<>();
+		result.clazz = clazz;
+		
+		result.methods = new HashMap<>();
+		Set<Method> methods = new HashSet<>(Arrays.asList(clazz.getDeclaredMethods()));
+		methods.addAll(Arrays.asList(clazz.getMethods()));
+		methods.removeIf(method->(!method.isAnnotationPresent(JSONRpcExtern.class)));
+		methods.forEach(method->{
+			System.out.println("Pulling metadata from '" + method + "'");
+			JSONRpcExtern methodAnnotation = method.getAnnotation(JSONRpcExtern.class);
+			
+			Map<String, ParamInfo> parameters = new HashMap<>();
+			for (AnnotatedType param : method.getAnnotatedParameterTypes()) {
+				System.out.println("Building info for " + param.getType() + Arrays.toString(param.getDeclaredAnnotations()));
+				JSONRpcParam info = param.getDeclaredAnnotation(JSONRpcParam.class);
+				System.out.println(info);
+				ParamInfo parameter = new ParamInfo();
+				parameter.annotation = info;
+				parameter.defaultValue = info.defaultValue();
+				parameter.jsonType = info.type();
+				parameter.name = info.value();
+				parameter.type = param;
+				parameters.put(info.value(), parameter);
+			}
+			result.methods.put(methodAnnotation.name(), new MethodInfo(method, methodAnnotation.name(), parameters, methodAnnotation));
+		});
+		return result;
+	}
+	
+	public static Set<Method> getMethodBySignature(Class<?> clazz, String methodName, String[] args, int maxArgs, Set<String> allowedExceptions) {
 		Set<Method> candidates = new HashSet<>();
 		
 		for (Method declared : clazz.getDeclaredMethods())
@@ -172,7 +305,8 @@ public abstract class JSONRpcClient {
 				candidates.add(method);
 		
 		if (args != null) {
-			candidates.removeIf((method)->(method.getParameterCount() > args.length));
+			int numArgs = maxArgs > 0 ? Math.min(args.length, maxArgs) : args.length;
+			candidates.removeIf(method->(method.getParameterCount() > numArgs));
 			for (Method candidate : candidates) {
 				int i = 0;
 				for (Class<?> param : candidate.getParameterTypes()) {
@@ -183,12 +317,29 @@ public abstract class JSONRpcClient {
 					
 					if (param.isPrimitive()) {
 						//try unboxing
+						Class<?> boxed = box(param);
+						if (boxed.getSimpleName().equals(arg) || boxed.getName().equals(arg))
+							continue;
+					} else {
 						Class<?> unboxed = unbox(param);
-						if (unboxed.getSimpleName().equals(arg) || unboxed.getName().equals(arg))
+						if (unboxed != param && (unboxed.getSimpleName().equals(arg) || unboxed.getName().equals(arg)))
 							continue;
 					}
+					
+					if (param.isArray()) {
+						Class<?> unboxed = unboxArray(param);
+						if (unboxed.getSimpleName().equals(arg) || unboxed.getName().equals(arg))
+							continue;
+						Class<?> boxed = boxArray(param);
+						if (boxed.getSimpleName().equals(arg) || boxed.getName().equals(arg))
+							continue;
+					}
+					candidates.remove(candidate);
+					break;
 				}
 			}
+		} else if (maxArgs > 0) {
+			candidates.removeIf(method->(method.getParameterCount() > maxArgs));
 		}
 		
 		for (Method candidate : candidates) {
@@ -203,12 +354,13 @@ public abstract class JSONRpcClient {
 		return candidates;
 	}
 	@SuppressWarnings("unchecked")
-	protected Class<? extends JSONRemote> generateProto(Class<? extends JSONRemote> clazz) throws NotFoundException, CannotCompileException {
+	public Class<? extends JSONRemote> generateProto(Class<? extends JSONRemote> clazz) throws NotFoundException, CannotCompileException {
 		CtClass result;
 		CtClass clazzCt = getCt(clazz);
 		String outClassName = clazz.getName() + "__RPC";
 		
 		if (clazz.isInterface()) {
+			System.out.println("Implementing " + clazz.getName() + " @ " + outClassName);
 			result = pool.makeClass(outClassName, GENERATED_IMPL_CLASS);
 			result.addInterface(JSON_REMOTE_INTERFACE);
 			result.addInterface(pool.get(clazz.getName()));
@@ -218,6 +370,7 @@ public abstract class JSONRpcClient {
 			constructor.setModifiers(Modifier.PUBLIC);
 			result.addConstructor(constructor);
 		} else {
+			System.out.println("Extending " + clazz.getName() + " @ " + outClassName);
 			//is abstract class JSONRpcGeneratedImplementation(JSONRpcClient, String)
 			result = pool.makeClass(outClassName, pool.get(clazz.getName()));
 			result.addInterface(RPC_IMPL_CLASS);
@@ -231,7 +384,7 @@ public abstract class JSONRpcClient {
 			result.addField(rpcUrlField);
 			
 			//Create constructor to invoke
-			result.addConstructor(CtNewConstructor.make(new CtClass[]{JSON_RPC_CLIENT_CLASS, STRING_CLASS}, new CtClass[]{}, "super();$0.__rpcClient = $1;$0.__rpcUrl=$2", result));
+			result.addConstructor(CtNewConstructor.make(new CtClass[]{JSON_RPC_CLIENT_CLASS, STRING_CLASS}, new CtClass[]{}, "{super();$0.__rpcClient = $1;$0.__rpcUrl=$2;}", result));
 			
 			//implement JSONRpcImplementation (generate getters)
 			CtMethod getClientMethod = CtNewMethod.getter("getClient", rpcClientField);
@@ -246,6 +399,7 @@ public abstract class JSONRpcClient {
 		//now generate methods
 		ClassInfo<?> classInfo = metadata.computeIfAbsent(clazz, this::getMeta);
 		for (Entry<String, MethodInfo> methodData : classInfo.methods.entrySet()) {
+			System.out.println("Implementing " + methodData.getKey());
 			MethodInfo methodInfo = methodData.getValue();
 			Method method = methodInfo.method;
 			
@@ -262,12 +416,12 @@ public abstract class JSONRpcClient {
 			String postparserSignature = methodInfo.annotation.postparser();
 			if (preparserSignature != null && !preparserSignature.isEmpty()) {
 				String preparserName = preparserSignature;
-				String[] preparserArgs;
-				String[] preparserExceptions;
+				String[] preparserArgs = null;
+				String[] preparserExceptions = null;
 				CtClass ownerClass = clazzCt;
 				
 				int idxToken;
-				if ((idxToken = preparserName.indexOf("#")) > 0) {
+				if ((idxToken = preparserName.indexOf("#")) > 0 || (idxToken = preparserName.indexOf(".")) > 0) {
 					ownerClass = getCt(preparserName.substring(0, idxToken));
 					preparserName = preparserName.substring(idxToken);
 				}
@@ -288,6 +442,11 @@ public abstract class JSONRpcClient {
 					
 					preparserArgs = preparserArgsText.split(",");//TODO fix for generics
 				}
+				
+				System.out.println("Preparser name: " + preparserName);
+				System.out.println("Preparser args: " + Arrays.toString(preparserArgs));
+				System.out.println("Preparser exceptions: " + Arrays.toString(preparserExceptions));
+				System.out.println("Preparser owner: " + ownerClass);
 				
 				//get preparser
 				for (Method declared : clazz.getDeclaredMethods())
@@ -321,7 +480,7 @@ public abstract class JSONRpcClient {
 			throw new IllegalAccessException("Illegal invocation of " + object.getClass() + '#' + methodName);
 		
 		//test if I can invoke it
-		Set<String> paramNames = params.keySet();
+//		Set<String> paramNames = params.keySet();
 		//TODO finish
 		/*if (!paramNames.equals(method.params)) {
 			if (!method.params.containsAll(paramNames)) {
