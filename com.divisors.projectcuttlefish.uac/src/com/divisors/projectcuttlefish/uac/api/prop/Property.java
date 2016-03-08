d9fb8d061852d888bf8161f030c40c64b07c0f79
mailmindlin <mailmindlin@users.noreply.github.com>
2016-03-08 14:44:45 -0500

    Update Property.java

--- com.divisors.projectcuttlefish.uac/src/com/divisors/projectcuttlefish/uac/api/prop/Property.java
+++ com.divisors.projectcuttlefish.uac/src/com/divisors/projectcuttlefish/uac/api/prop/Property.java
@@ -2,5 +2,5 @@ package com.divisors.projectcuttlefish.uac.api;
 
 public interface Property {
 	PropertyReference getReference();
-	boolean updatedSince(Instant timestamp);
+	boolean modifiedSince(Instant timestamp);
 }
