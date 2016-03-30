1169a1f1021c3b2c4cd6fc53e2933056236f2258
mailmindlin <liamttlyrox@gmail.com>
2016-03-30 17:44:06 -0400

    Basically rewrote plugin system

--- com.divisors.projectcuttlefish.contentmanager/src/com/divisors/projectcuttlefish/contentmanager/api/plugins/Plugin.java
+++ com.divisors.projectcuttlefish.contentmanager/src/com/divisors/projectcuttlefish/contentmanager/api/plugins/Plugin.java
@@ -1,25 +1,106 @@
 package com.divisors.projectcuttlefish.contentmanager.api.plugins;
 
-public interface Plugin {
+import static com.divisors.projectcuttlefish.httpserver.api.tcp.SubsetSelector.$t;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.script.ScriptEngine;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.divisors.projectcuttlefish.httpserver.api.Version;
+
+import reactor.bus.Event;
+import reactor.bus.EventBus;
+import reactor.bus.registry.Registration;
+import reactor.fn.Consumer;
+import reactor.fn.tuple.Tuple;
+
+/**
+ * 
+ * @author mailmindlin
+ */
+public class Plugin implements EventTarget {
+	/**
+	 * Convert a JSONArray of strings to a list of strings
+	 * @param arr JSONArray
+	 * @return list of strings
+	 */
+	protected static List<String> stringArrayToList(JSONArray arr) {
+		if (arr == null)
+			return Collections.emptyList();
+		ArrayList<String> result = new ArrayList<>(arr.length());
+		for (Object entry : arr)
+			result.add(entry.toString());
+		return result;
+	}
+	protected final String name;
+	protected final Version version;
+	protected final String description;
+	protected final Path homeDir;
+	protected final List<String> scripts;
+	protected final List<String> permissions;
+	protected final List<String> optionalPermissions;
+	protected final List<String> storage;
+	protected final long id;
+	protected ScriptEngine engine;
+	protected EventBus bus;
+	public Plugin(JSONObject data, Path home, long id, ScriptEngine engine, EventBus bus) {
+		this.name = data.getString("name");
+		this.version = new Version(data.getString("version"));
+		this.description = data.optString("description", "[no description]");
+		this.homeDir = home;
+		this.scripts = stringArrayToList(data.optJSONArray("scripts"));
+		JSONObject permissions = data.optJSONObject("permissions");
+		if (permissions != null) {
+			this.permissions = stringArrayToList(permissions.optJSONArray("required"));
+			this.optionalPermissions = stringArrayToList(permissions.optJSONArray("optional"));
+		} else {
+			this.permissions = this.optionalPermissions = Collections.emptyList();
+		}
+		this.storage = stringArrayToList(data.optJSONArray("storage"));
+		this.id = id;
+		this.engine = engine;
+		this.bus = bus;
+	}
 	/**
 	 * Get this plugin's name
 	 * @return name
 	 */
-	String getName();
-	String getVersion();
-	String getDescription();
-	void load();
-	void activate();
-	void deactivate();
-	void unload();
-	default void beforeUpgradeFrom() {}
-	default void upgradeFrom() {
-		deactivate();
+	public String getName() {
+		return this.name;
 	}
-	default void afterUpgradeFrom() {}
-	default void beforeUpgradeTo() {}
-	default void upgradeTo() {
-		activate();
+	public Version getVersion() {
+		return this.version;
 	}
-	default void afterUpgradeTo() {}
+	public String getDescription() {
+		return this.description;
+	}
+	public long getId() {
+		return this.id;
+	}
+	public EventBus getBus() {
+		return this.bus;
+	}
+	public ScriptEngine getEngine() {
+		return this.engine;
+	}
+	public Path getHome() {
+		return this.homeDir;
+	}
+	public void triggerEvent(String name, Object...args) {
+		System.out.println("PLUGIN " + getId() + ":\tInvoking event " + name);
+		bus.notify(Tuple.of("plugins",getId(),name), new Event<>(args));
+	}
+	@Override
+	public void addEventListener(String eventName, Consumer<? extends Event<?>> handler, boolean onlyOnce) {
+		System.out.println("PLUGIN " + getId() + ":\tAdding event listener for " + eventName);
+		 Registration<Object, Consumer<? extends Event<?>>> registration = getBus().on($t("plugins",getId(),eventName), handler);
+		 if (onlyOnce)
+			 registration.cancelAfterUse();
+	}
 }
--- com.divisors.projectcuttlefish.contentmanager/src/com/divisors/projectcuttlefish/contentmanager/api/plugins/PluginManager.java
+++ com.divisors.projectcuttlefish.contentmanager/src/com/divisors/projectcuttlefish/contentmanager/api/plugins/PluginManager.java
@@ -1,39 +1,145 @@
 package com.divisors.projectcuttlefish.contentmanager.api.plugins;
 
-import java.nio.file.Path;
-import java.util.concurrent.ConcurrentHashMap;
+import static com.divisors.projectcuttlefish.httpserver.api.tcp.SubsetSelector.$t;
 
+import java.io.BufferedReader;
+import java.io.CharConversionException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.CharacterCodingException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import javax.swing.text.ChangedCharSetException;
 
-import jdk.nashorn.api.scripting.NashornScriptEngine;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
+import reactor.bus.Event;
+import reactor.bus.EventBus;
+import reactor.fn.tuple.Tuple;
+
+/**
+ * 
+ * @author mailmindlin
+ */
 public class PluginManager {
-	public static final PluginManager INSTANCE = new PluginManager();
+	public static PluginManager INSTANCE;
 	
-	protected ConcurrentHashMap<String, Plugin> plugins = new ConcurrentHashMap<>();
-	public void loadPlugin(Path json) {
+	protected final AtomicLong nextID = new AtomicLong(0);
+	protected ConcurrentHashMap<Long, Plugin> plugins = new ConcurrentHashMap<>();
+	protected ExecutorService executor;
+	protected EventBus bus;
+	public PluginManager(EventBus bus) {
+		this.bus = bus;
+		bus.on($t("plugins.scriptinit"), this::loadScripts);
+	}
+	public boolean loadPlugin(Path json) {
+		System.out.println("Attempting to load manifest from " + json);
 		Path home = json.getParent();
+		JSONObject manifestData;
+		try (BufferedReader br = Files.newBufferedReader(json)) {
+			manifestData = new JSONObject(new JSONTokener(br));
+		} catch (FileNotFoundException e) {
+			System.err.println("Unable to find manifest file @ " + json);
+			e.printStackTrace();
+			return false;
+		} catch (ChangedCharSetException | CharacterCodingException | CharConversionException | UnsupportedEncodingException e){
+			System.err.println("There was a problem with parsing the text in the manifest file @ " + json);
+			e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			System.err.println("There was a problem reading the manifest file @ " + json);
+			e.printStackTrace();
+			return false;
+		} catch (JSONException e) {
+			System.err.println("Unable to parse manifest @ " + json + " (is it valid JSON?)");
+			e.printStackTrace();
+			return false;
+		}
+		System.out.println(manifestData);
+		long id = nextID.getAndIncrement();
+		ScriptEngine engine = getEngine();
+		Plugin p = new Plugin(manifestData, home, id, engine, bus);
+		this.plugins.put(id, p);
+		engine.put("Plugin", p);
+		System.out.println("Initializing plugin #" + id + ": " + p.getName());
+		bus.notify(Tuple.of("plugins.scriptinit", id));
+		System.out.println("Done initializing");
+		return true;
+	}
+	public void loadScripts(Event<?> ev) {
+		System.out.print("Loading plugin #");
+		long id = (Long) ((Tuple)ev.getKey()).get(1);
 		
+		Plugin plugin = this.getPluginByID(id);
+		System.out.println(plugin.getId() + "(" + plugin.getName() + ")");
+		for (String script : plugin.scripts) {
+			Path scriptPath;
+			try {
+				scriptPath = plugin.getHome().resolve(script).normalize();
+			} catch (InvalidPathException e) {
+				System.err.println("Invalid script path " + script + " (Plugin #" + plugin.getId() + ")");
+				e.printStackTrace();
+				continue;
+			}
+			System.out.println("Loading script @ " + scriptPath);
+			if (!checkPluginFileAccess(plugin, scriptPath)) {
+				System.err.println("Invalid permissions to load script from " + scriptPath + " (Plugin #" + plugin.getId() + ")");
+				System.err.println("If you need to load this script, you can request the permission 'file.script.loadSpecial'");
+				continue;//should it die?
+			}
+			try (BufferedReader br = Files.newBufferedReader(scriptPath)) {
+				plugin.getEngine().eval(br);
+			} catch (FileNotFoundException e) {
+				System.err.println("Unable to find script @ " + scriptPath + " (Pluugin #" + plugin.getId() + ")");
+				e.printStackTrace();
+				continue;
+			} catch (IOException e) {
+				e.printStackTrace();
+				continue;
+			} catch (ScriptException e) {
+				e.printStackTrace();
+			}
+		}
+		plugin.triggerEvent("load");
 	}
-	public void loadJS(Path p) {
+	public Map<Long, Plugin> getPlugins() {
+		return Collections.unmodifiableMap(this.plugins);
 	}
-
-	public void loadJS(String js) throws ScriptException {
-		NashornScriptEngine engine = getEngine();
-		engine.eval(js);
+	public boolean checkPluginFileAccess(Plugin plugin, Path path) {
+		return path.startsWith(plugin.getHome());
 	}
-
-	protected NashornScriptEngine getEngine() {
-		NashornScriptEngine engine = (NashornScriptEngine) new ScriptEngineManager().getEngineByName("Nashorn");
+	public String loadTextFile(Path path) throws FileNotFoundException, IOException {
+		try (BufferedReader br = Files.newBufferedReader(path)) {
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = br.readLine()) != null)
+				sb.append(line.trim()).append("\n");
+			return sb.toString();
+		}
+	}
+	public Plugin getPluginByID(long id) {
+		return this.plugins.get(id);
+	}
+	protected ScriptEngine getEngine() {
+		System.out.print("Getting engine...");
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("Nashorn");
 		engine.put("PluginManager", this);
-		engine.put("Plugin", Plugin.class);
+		engine.put("console", Console.getInstance());
+		System.out.println("SUCCESS");
 		return engine;
-	}
-	
-	public void register(Plugin proto) {
-		plugins.put(proto.getName(), proto);
-		System.out.println("Loaded plugin " + proto.getName());
-		proto.load();
 	}
 }
