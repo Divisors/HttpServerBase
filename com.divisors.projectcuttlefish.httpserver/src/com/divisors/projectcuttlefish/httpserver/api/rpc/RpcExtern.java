f1b910a505c741abb4f00bb34ecfaf232a3c846d
mailmindlin <mailmindlin@users.noreply.github.com>
2016-05-18 13:08:32 -0400

    Update and rename JSONRpcExtern.java to RpcExtern.java

--- com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/api/rpc/RpcExtern.java
+++ com.divisors.projectcuttlefish.httpserver/src/com/divisors/projectcuttlefish/httpserver/api/rpc/RpcExtern.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface JSONRpcExtern {
+public @interface RpcExtern {
 	String name() default "";
 	JSONRpcAccessibility accessibility() default JSONRpcAccessibility.SECURED;
 	String preparser() default "";
