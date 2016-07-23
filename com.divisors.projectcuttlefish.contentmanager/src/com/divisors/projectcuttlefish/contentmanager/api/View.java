af9b131023ea12b4ea381542e8a17e9f730a11bd
mailmindlin <liamttlyrox@gmail.com>
2016-07-23 19:04:35 -0400

    Removed View framework

--- com.divisors.projectcuttlefish.contentmanager/src/com/divisors/projectcuttlefish/contentmanager/api/View.java
+++
@@ -1,5 +0,0 @@
-package com.divisors.projectcuttlefish.contentmanager.api;
-
-public interface View {
-	String getName();
-}
--- com.divisors.projectcuttlefish.contentmanager/src/com/divisors/projectcuttlefish/contentmanager/api/ViewHttpServlet.java
+++
@@ -1,8 +0,0 @@
-package com.divisors.projectcuttlefish.contentmanager.api;
-
-import javax.servlet.annotation.WebServlet;
-
-@WebServlet(description="Servlet for views", name="ViewHttpServlet", value={"/view/*"}, asyncSupported=true)
-public class ViewHttpServlet {
-
-}
